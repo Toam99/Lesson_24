dc8874101603bfe40c73feefee7af5cfa5ab6494
Toam99 <60461717+Toam99@users.noreply.github.com>
2020-06-18 17:14:24 +0800

    Add simple name parameters validation

--- app.rb
+++ app.rb
@@ -7,7 +7,8 @@ get '/' do
 	   erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School !!!</a>"			
 end
 
-get '/about' do      
+get '/about' do
+    @error = 'something wrong!'      
     erb :about
 end
 
@@ -26,6 +27,11 @@ post '/visit' do
     @datetime = params[:datetime]
     @barber = params[:barber]
     @color = params[:color]
+
+    if @username == ''
+        @error = 'Enter your name'
+        return erb :visit                
+    end
 
     erb "OK, username is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
 
